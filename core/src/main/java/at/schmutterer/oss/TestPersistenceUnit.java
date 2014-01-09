a3bb4e291ba5aca7a541ae1b7c34a46b395b556a
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2014-01-09 10:34:47 +0100

    rename to at.schmutterer.oss namespace

--- core/src/main/java/at/schmutterer/oss/TestPersistenceUnit.java
+++ core/src/main/java/at/schmutterer/oss/TestPersistenceUnit.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openengsb.labs.jpatest.junit;
+package at.schmutterer.oss;
 
 import java.io.IOException;
 import java.io.InputStream;
